3b01753a6eae1677737700758bf8eeaac7acc807
Isabella Ekdahl <isabellaekdahl@outlook.com>
2022-11-30 17:15:52 +0100

    add context and missing models

---
+++ Server/Models/Contact.cs
@@ -0,0 +1,18 @@
+namespace NeverAlone.Models
+{
+
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public int UserId { get; set; }
+
+        public User user { get; set; }
+
+
+    }
+}
