2d07082d22a25b7b32e05a367bf8d6e6f2829f5f
isabellaes <borjessonisabella@gmail.com>
2023-01-11 18:08:17 +0100

    create expo project

---
+++ Client/Screens/EditProfileScreen.tsx
@@ -0,0 +1,11 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function EditProfileScreen() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
