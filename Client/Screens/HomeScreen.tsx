d94d95113d44141c5cf922c89f1729714a6edd85
Eva-Li8E <evaliboden@gmail.com>
2023-01-17 15:11:10 +0100

    fix mergeconflict

--- Client/Screens/HomeScreen.tsx
+++ Client/Screens/HomeScreen.tsx
@@ -12,7 +12,6 @@ export default function HomeScreen({ navigation }: Props) {
     <View>
       <Text style={{fontSize:25, marginBottom:25}}>Home Screen</Text>
       <Link to="/Login"><Text>Tryck här på länken för att komma till Login</Text></Link>
-      <Link to="/EditProfile"><Text>Tryck här på länken för att komma till EditProfile</Text></Link>
       <Text style={{fontSize:20, marginTop:25}}>Tänker att denna sidan får innehålla det som är på  översikt sidan? </Text>
     </View>
   );
