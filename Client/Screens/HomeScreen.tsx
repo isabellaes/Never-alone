4e549949597e502e1b2f882bb4d74ca0fc7bb086
Eva-Li8E <evaliboden@gmail.com>
2023-01-17 14:58:02 +0100

    created editprofile style and some fix

--- Client/Screens/HomeScreen.tsx
+++ Client/Screens/HomeScreen.tsx
@@ -6,11 +6,13 @@ import { RootStackParamList } from "../navigation/RootNavigator";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Home">;
 
+
 export default function HomeScreen({ navigation }: Props) {
   return (
     <View>
       <Text style={{fontSize:25, marginBottom:25}}>Home Screen</Text>
       <Link to="/Login"><Text>Tryck här på länken för att komma till Login</Text></Link>
+      <Link to="/EditProfile"><Text>Tryck här på länken för att komma till EditProfile</Text></Link>
       <Text style={{fontSize:20, marginTop:25}}>Tänker att denna sidan får innehålla det som är på  översikt sidan? </Text>
     </View>
   );
