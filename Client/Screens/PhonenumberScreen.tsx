cfd94b2808dc1ac8e0c0256fcb0ec743573b4569
Eva-Li8E <evaliboden@gmail.com>
2023-02-18 10:14:46 +0100

    updated phone

--- Client/Screens/PhonenumberScreen.tsx
+++ Client/Screens/PhonenumberScreen.tsx
@@ -1,26 +1,74 @@
 import { Link } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
-import { View, Text } from "react-native";
-//import { BottomBar } from "../Componets/BottomBar";
+import React, { useState } from "react";
+import { View, Text, ScrollView , StyleSheet} from "react-native";
+import Meditation from "../Componets/Meditation";
+import OpenURL from "../Componets/OpenUrl";
+
 import { RootStackParamList } from "../navigation/RootNavigator";
 
-type Props = NativeStackScreenProps<RootStackParamList, "PhoneNumber">;
+// type Props = NativeStackScreenProps<RootStackParamList, "PhoneNumber">;
 
-export default function PhonenumberScreen({ navigation, route }: Props) {
+interface ContactProps {
+  contacts: {
+    name: string,
+    number: number,
+    url: string
+  } []
+
+}
+
+export default function PhonenumberScreen() {
+   const [contakt, setContakt] = useState<ContactProps["contacts"]>([
+    {
+      name: "Barn & ungdomspsykiatrin",
+      number: 1177,
+      url: "https://www.1177.se/Vastra-Gotaland/barn--gravid/vard-och-stod-for-barn/bup--barn--och-ungdomspsykiatrin/"
+    },
+    {
+      name: "VuxenPsykiatrin",
+      number: 1177,
+      url: "https://www.1177.se/Vastra-Gotaland/liv--halsa/psykisk-halsa/att-soka-stod-och-hjalp/soka-psykiatrisk-vard/"
+    },
+    {
+      name: "test3",
+      number: 123456,
+      url: "https://www.1177.se/Vastra-Gotaland/barn--gravid/vard-och-stod-for-barn/bup--barn--och-ungdomspsykiatrin/"
+    }
+
+   ])
+
   return (
-    <View>
-      <View>
-        <Text style={{ fontSize: 25, marginBottom: 25 }}>
-          Phonenumber Screen
-        </Text>
-        <Link to="/Storys" style={{ marginBottom: 25 }}>
-          <Text>Tryck här på länken för att komma till Storys</Text>
-        </Link>
-        <Link to="/Home">
-          <Text>Tryck här på länken för att komma till Home</Text>
-        </Link>
-      </View>
+    <View style={styles.container}>
+      <ScrollView style={{ width: "95%" }}>
+        { contakt.map(c=> (
+          <View>
+            <Text style={styles.citat}>{c.name} Telefonnummer:{c.number} 
+             <OpenURL url={c.url}></OpenURL>
+            </Text>
+          </View>
+        ))}
+
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  citat: {
+    fontSize: 17,
+    textAlign: "center",
+    padding: 15,
+  },
+});
+
+
