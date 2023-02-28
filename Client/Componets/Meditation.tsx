bc267995cd77489ad0a08d7769fcdeedf8913767
Eva-Li8E <evaliboden@gmail.com>
2023-02-28 20:44:54 +0100

    fixed meditationscreen

--- Client/Componets/Meditation.tsx
+++ Client/Componets/Meditation.tsx
@@ -1,12 +1,15 @@
 import React, {useCallback} from 'react';
 import {Alert, Button, Linking, StyleSheet, View, Text} from 'react-native';
 
+
+
 const LearnMeditationURL = 'https://www.mindful.org/how-to-meditate/';
 const MeditationURL = 'https://www.youtube.com/results?search_query=meditation';
 
 type OpenURLButtonProps = {
   url: string;
   children: string;
+
 };
 
 const OpenURLButton = ({url, children}: OpenURLButtonProps) => {
@@ -21,15 +24,16 @@ const OpenURLButton = ({url, children}: OpenURLButtonProps) => {
     }
   }, [url]);
 
-  return <Button title={children} onPress={handlePress} />;
+  return <Button color="#B69EC3" title={children} onPress={handlePress} />;
 };
 
 const Meditation = () => {
   return (
     <View style={styles.container}>
-      <OpenURLButton url={LearnMeditationURL}>Lär dig mer om meditation</OpenURLButton>
-      <Text style={styles.text}>Landa i nuet en stund ❤️</Text>
+      <Text style={styles.text}></Text>
       <OpenURLButton url={MeditationURL}>Direkt till meditation</OpenURLButton>
+       <Text style={styles.texttvå}> eller </Text>
+      <OpenURLButton  url={LearnMeditationURL}>Lära dig mer om meditation</OpenURLButton>
     </View>
   );
 };
@@ -38,14 +42,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+   
   },
   text: {
-    marginBottom:20,
-    marginTop:20,
-    fontSize: 18
+   
+   
     
-  }
+  },
+  texttvå: {
+   marginBottom: 5,
+    fontSize: 20
+  },
 });
 
 export default Meditation;
--- Client/Screens/MeditationScreen.tsx
+++ Client/Screens/MeditationScreen.tsx
@@ -7,6 +7,8 @@ import { useAppDispatch } from "../store/store";
 import OverviewCard from "../Componets/OverviewCard";
 import Meditation from "../Componets/Meditation";
 import { BottomBar } from "../Componets/BottomBar";
+import Weather from "../Componets/Weather";
+
 //import { styles } from "../utils/styleSheet";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Meditation">;
@@ -16,16 +18,22 @@ export default function MeditationScreen({ navigation, route }: Props) {
     <View style={styles.container}>
       <ScrollView style={{ width: "95%" }}>
         <Text style={styles.title}>
-          Sätt dig eller lägg dig ner bekvämt och koppla av lite, username
+          Landa eller kanske bara ladda om en stund..
         </Text>
         <Image
           style={styles.image}
           source={{
-            uri: "https://img.freepik.com/free-photo/meditation_1048-1840.jpg",
+            uri: "https://cdn.pixabay.com/photo/2020/03/21/19/27/sea-4955005_960_720.jpg",
           }}
         ></Image>
-
+        <Text style={styles.titles}>
+          Meditera i lugn och ro.. 
+        </Text>
         <Meditation></Meditation>
+        <Text style={styles.titlestvå}>
+          ..hörlurarna i öronen och gå en lugn promenad..</Text>
+        <Weather></Weather>
+
       </ScrollView>
       <BottomBar navigation={navigation} route={route}></BottomBar>
     </View>
@@ -40,19 +48,29 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "center",
     alignContent: "center",
-    justifyContent: "space-between",
   },
 
   title: {
     textAlign: "center",
+    fontSize: 23,
+   
+    padding: 10
+  },
+  titles: {
+    textAlign: "center",
     fontSize: 20,
-    padding: 5,
-    marginTop: 20,
-    marginBottom: 20,
+    marginTop: 10
+  
+  },
+  titlestvå: {
+    textAlign:"center",
+    fontSize: 20,
+    marginTop: 20
+  
   },
   image: {
     width: "100%",
-    height: 300,
-    marginBottom: 40,
+    height: 175,
+    marginBottom: 10,
   },
 });
