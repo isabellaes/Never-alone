540218ff2cc609f319f13efc1c3b3d5b77225dfc
Eva-Li8E <evaliboden@gmail.com>
2023-03-12 19:03:14 +0100

    updated editscreen

--- Client/Componets/ButtonCamera.tsx
+++ Client/Componets/ButtonCamera.tsx
@@ -45,8 +45,8 @@ export default function ButtonCamera({ label, theme, onPress }: any) {
 }
 const styles = StyleSheet.create({
   buttonContainer: {
-    width: 320,
-    height: 68,
+    width: 120,
+    height: 40,
     marginHorizontal: 20,
     alignItems: "center",
     justifyContent: "center",
